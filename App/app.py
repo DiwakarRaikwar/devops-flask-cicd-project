d3741bffccbf88966d1815bd62580006797b6d8e
Diwakar <raikwarempire001@gmail.com>
2026-03-15 16:56:13 +0530

    Update flask output

--- App/app.py
+++ App/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import socket
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Diwakar! DevOps CI/CD project running 🚀"
+    return f"Hello from Diwakar CI/CD Project!<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
