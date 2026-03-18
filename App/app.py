6f3487de854ae9ea15a4408ecd522bc8e9e4accc
Diwakar <raikwarempire001@gmail.com>
2026-03-19 01:32:34 +0530

    Day-6: Added Docker Compose with Redis integration

--- App/app.py
+++ App/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import redis
 import socket
 import datetime
 
 app = Flask(__name__)
 
+r = redis.Redis(host='redis', port=6379)
+
 @app.route("/")
 def home():
-    return f"Hello from Diwakar CI/CD Project!<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
+    count = r.incr('hits')
+    return f"Hello Diwakar!<br>Visits: {count}<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
