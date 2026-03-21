96e265dc6a3683a1e55af9c651779e86dee99821
Diwakar <raikwarempire001@gmail.com>
2026-03-21 18:56:44 +0530

    Test auto deployment

--- App/app.py
+++ App/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello Diwakar!<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
+    return f"Deployed by Diwakar 🚀!<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
