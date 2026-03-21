56842fe6b19e0912dd95de4b457cfe188436aca0
Diwakar <raikwarempire001@gmail.com>
2026-03-21 17:00:04 +0530

    Removed Redis dependencny for Ec2 Deployment

--- App/app.py
+++ App/app.py
@@ -1,16 +1,12 @@
 from flask import Flask
-import redis
 import socket
 import datetime
 
 app = Flask(__name__)
 
-r = redis.Redis(host='redis', port=6379)
-
 @app.route("/")
 def home():
-    count = r.incr('hits')
-    return f"Hello Diwakar!<br>Visits: {count}<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
+    return f"Hello Diwakar!<br>IP: {socket.gethostbyname(socket.gethostname())}<br>Time: {datetime.datetime.now()}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
